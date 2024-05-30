a44d2078624d2b7401e969842b30d460879d2e2c
Lóránt Pintér <lorant@gradle.com>
2024-05-30 16:29:21 +0200

    Register a task

--- plugin/src/main/java/org/example/PublishedApiTestPlugin.java
+++ plugin/src/main/java/org/example/PublishedApiTestPlugin.java
@@ -6,5 +6,10 @@ import org.gradle.api.Plugin;
 public class PublishedApiTestPlugin implements Plugin<Project> {
     public void apply(Project project) {
         System.out.println("Hello from PublishedApiTestPlugin");
+        project.getTasks().register("publishedApiTest", task -> {
+            task.doLast(__ -> {
+                System.out.println("Hello from PublishedApiTestPlugin task");
+            });
+        });
     }
 }
