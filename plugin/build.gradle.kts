d6f7d468d5f45aacbbdb2be5698692264af15d37
Lóránt Pintér <lorant@gradle.com>
2024-05-30 15:48:17 +0200

    Make compilation fail without Gradle API

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,20 @@
 plugins {
     `java-library`
 }
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    // We need at least one dependency to make sure Java does not
+    // See https://github.com/gradle/gradle/pull/29343
+    implementation("com.google.guava:guava:33.2.0-jre")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
